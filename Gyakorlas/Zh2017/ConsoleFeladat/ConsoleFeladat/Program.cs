da8f7c4a64680ca52681d51a0a6e1f9bab421592
Laci <mlacko020523@gmail.com>
2023-04-23 03:38:19 +0200

    Console

---
+++ Gyakorlas/Zh2017/ConsoleFeladat/ConsoleFeladat/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace ConsoleFeladat
+{
+    internal class Program
+    {
+
+        private static long value;
+        private static object valueLock = new object();
+        private static AutoResetEvent AutoResetEvent = new AutoResetEvent(false);
+        private static Random _random = new Random();
+
+        static void Main(string[] args)
+        {
+            elso();
+            harmadik();
+        }
+
+        private static void harmadik(){
+            Thread t = new Thread(MainThread); t.Start();
+
+            Thread f1 = new Thread(ThreadFunc) { IsBackground = true, Name= "f1" }; f1.Start();
+            Thread f2 = new Thread(ThreadFunc) { IsBackground = true, Name = "f2" }; f2.Start();
+            Thread f3 = new Thread(ThreadFunc) { IsBackground = true, Name = "f3" }; f3.Start();
+            Thread f4 = new Thread(ThreadFunc) { IsBackground = true, Name = "f4" }; f4.Start();
+            Thread f5 = new Thread(ThreadFunc) { IsBackground = true, Name = "f5" }; f5.Start();
+        }
+
+
+        private static void ThreadFunc(){
+            AutoResetEvent.WaitOne();
+            lock (valueLock) {
+                Console.WriteLine(value +" "+ Thread.CurrentThread.Name);
+            }
+            //Thread.CurrentThread.Join();
+        }
+
+        private static void MainThread(){
+            while(true){
+                lock(valueLock){
+                    value = _random.Next();
+                    AutoResetEvent.Set();
+                }
+                Thread.Sleep(1000);
+            }
+        }
+
+
+        private static void elso(){
+            Progress progress = new Progress();
+            progress.ProgressChange += (x, y) => { int c = y - x; Console.WriteLine("A változás értéke: {0}", c); };
+            progress.Progress_ = 10;
+            progress.Progress_ = 5;
+            progress.Progress_ = 6;
+
+
+
+        }
+    }
+}
---
+++ Gyakorlas/Zh2017/ConsoleFeladat/ConsoleFeladat/Progress.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleFeladat
+{
+    public delegate void ProgressChangeDelegate(int regi, int uj);
+    public class Progress
+    {
+        
+        private int progressPercent;
+        public int Progress_{
+
+            get => progressPercent;
+            set
+            {
+                if (value >= 0 && value <= 100)
+                {
+                    ProgressChange?.Invoke(progressPercent, value);
+
+                    progressPercent = value;
+                }
+            }
+        }
+        public event ProgressChangeDelegate ProgressChange;
+
+
+    }
+}
